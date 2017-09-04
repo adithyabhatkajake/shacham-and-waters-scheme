85a21f4589665f92b02856896a31de90d11f103b
HermitSage <dth.bht@gmail.com>
2017-09-04 17:04:17 +0530

    Modularized the code to look better and cleaner. Seperated BLS related functions and updated the dependencies in the makefile.

--- main.c
+++ main.c
@@ -2,27 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "bls.h"
+
 /**
  *  Program to verify BLS Signatures
  */
-
-#define SIGN_OK 1
-#define SIGN_FAIL 0
-
-int verify_signature(pairing_t pairing,
-    element_t group1, element_t group2,
-    element_t signature,
-    element_t public_key)
-{
-    element_t temp1, temp2;
-
-    element_init_GT(temp1, pairing);
-    element_init_GT(temp2, pairing);
-
-    pairing_apply(temp1, signature, group2, pairing);
-    pairing_apply(temp2, group1, public_key, pairing);
-    return element_cmp(temp1, temp2)?SIGN_FAIL:SIGN_OK;
-}
 
 int main(int argc, char const *argv[])
 {
